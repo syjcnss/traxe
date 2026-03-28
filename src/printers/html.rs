a086efd72e0fc5ad219947806058867e8ccd0044
syjcnss <syjcnss@gmail.com>
2026-03-28 15:47:07 +0800

    Fix html printer dark mode

--- src/printers/html.rs
+++ src/printers/html.rs
@@ -50,8 +50,8 @@ const TEMPLATE: &str = r###"<!DOCTYPE html>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>trace: PLACEHOLDER_TX_HASH</title>
-  <script>tailwind.config = { darkMode: 'class' }</script>
   <script src="https://cdn.tailwindcss.com"></script>
+  <script>tailwind.config = { darkMode: 'class' }</script>
   <style>
     ::-webkit-scrollbar { width: 6px; height: 6px; }
     html.dark ::-webkit-scrollbar-track { background: #09090b; }
